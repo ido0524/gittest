77e8b5cf06b27f5b8ca4facdde98019ce9941394
zhangsan <zhangsan@itcast.cn>
2019-04-23 19:47:57 +0800

    第一次提交

---
+++ src/cn/itcast/test/testgit.java
@@ -0,0 +1,13 @@
+package cn.itcast.test;
+
+/**
+ * @author duchengjun@itcast.cn
+ * @date 2019/4/23
+ */
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("1234");
+        System.out.println("12345");
+    }
+}
